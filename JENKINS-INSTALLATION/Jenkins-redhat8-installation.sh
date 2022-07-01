beb83a380271a45a008f2a7e83d554ec32b6c6db
Imoni <99836107+Sedibaba@users.noreply.github.com>
2022-07-01 04:29:09 -0600

    Create Jenkins-redhat8-installation.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-installation.sh
@@ -6,8 +6,11 @@
 cd /opt
 # 1. Install Java and other pre-requisits. 
 sudo yum -y install unzip wget tree git
-sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
-sudo yum install jdk-8u131-linux-x64.rpm -y
+del--- sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/11.0.15/d54c1d3a095b4ff2b6607d096fa80163/jdk-11.0.15-linux-x64.rpm
+
+******sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/java/18/latest/jdk-18_linux-aarch64_bin.rpm
+del --- sudo yum install jdk-11.0.15-linux-x64.rpm -y
+*****sudo yum install jdk-18_linux-aarch64_bin.rpm -y
 # 2. Add Jenkins Repository and key
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 cd /etc/yum.repos.d/
@@ -17,3 +20,13 @@ sudo yum -y install jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 sudo systemctl status jenkins
+
+
+ht
+
+https://www.oracle.com/ca-en/java/technologies/javase/jdk11-archive-downloads.html#license-lightbox
+
+
+  
+  
+  sudo wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" https://www.oracle.com/ca-en/java/technologies/javase/jdk11-archive-downloads.html
